64ea00092a58e6bfc68b8fa1abc88828aa4f9f51
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-06-07 10:10:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+Inspiration: Hey Duggie. ;)
+to generate the random pixel locations i just used the google random number generator 
+Note: yes the clouds are a bit weird.
+- to add.
+-: sun
+- : palm tree
+: leaves
+: bucket
+: spade
+: shells
+: bird 
+
+CSS Leaf from here: https://jsfiddle.net/xwvyo1c5/
+*/
